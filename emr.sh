4a8d581bed94d2ced3f3137a159c443c63ef5fa8
dubdabasoduba <dubdabasoduba@gmail.com>
2014-08-08 10:32:36 +0300

    try

--- emr.sh
+++ emr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export MAVEN_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5010 $MAVEN_OPTS"
 cd /media/benjamin/dubdabasoduba/Project/DMS
-mvn jetty:run
+mvn jetty:run  dd
