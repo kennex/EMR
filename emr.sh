42d5534a8ea9034bb6973aaa18f79e72e17410f1
dubdabasoduba <dubdabasoduba@gmail.com>
2014-08-21 09:26:37 +0300

    project location change

--- emr.sh
+++ emr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export MAVEN_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5010 $MAVEN_OPTS"
-cd /media/benjamin/dubdabasoduba/Project/EMR
+cd /home/benjamin/Project/EMR
 mvn jetty:run
